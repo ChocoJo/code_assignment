748b58ed944b08913c7496ac882c29a0dec55a38
Johanna <admin@Johannas-MacBook-Pro.local>
2021-06-01 12:42:29 +0200

    add navigationbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigation } from "./components/navigation/Navigation";
+import './utils/css/Global.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation/>
     </div>
   );
 }
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import tretton37_logga from '../../utils/images/logo/tretton37_logga.png';
+import './Navigation.css';
+
+export const Navigation = () => {
+    return (
+        <nav className="navbar">
+            <a className="navbar-brand" href="/" tabIndex="0">
+              <img className="logo" src={tretton37_logga} alt="Tretton 37 loga, redirect to home view when clicked"/>
+            </a>
+        </nav>
+    )
+}
